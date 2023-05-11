6631ebb1f2487e6b62043aed954915730425d88e
Marcel Hughes <marcellondon@icloud.com>
2023-05-10 23:25:35 -0500

    bug fixes

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let thisCalculation = []
 let thisJoinedCalculation
 let solution
 let runningCalculation = []
+let operators = ["+", "-", "*", "/"]
 
 
 function addComma(num) {
@@ -26,63 +27,49 @@ function addComma(num) {
 function calculate(input) {
     //    clear function
     if (input === 'C') {
-        runningCalculation = []
-        thisCalculation = []
+        runningCalculation = [0]
+        thisCalculation = [0]
         thisJoinedCalculation = 
-        solution = 
+        solution = [0]
         displayScreen.textContent = "CLEARED"
-    } else if (input === '+') {
-        // running calc string is pushed into operand
-        console.log(`operator ${input} pressed`)
-        runningCalculation.push(thisJoinedCalculation)
-        thisJoinedCalculation = runningCalculation.join('')
-        solution = eval(thisJoinedCalculation)
-        runningCalculation = [solution, input]
-        thisCalculation = []
-        thisJoinedCalculation = []
-        displayScreen.textContent = addComma(solution)
-    } else if (input === '-') {
-        console.log(`operator ${input} pressed`)
-        runningCalculation.push(thisJoinedCalculation)
-        thisJoinedCalculation = runningCalculation.join('')
-        solution = eval(thisJoinedCalculation)
-        runningCalculation = [solution, input]
-        thisCalculation = []
-        thisJoinedCalculation = []
-        displayScreen.textContent = addComma(solution)
-    } else if (input === '*') {
-        console.log(`operator ${input} pressed`)
-        runningCalculation.push(thisJoinedCalculation)
-        thisJoinedCalculation = runningCalculation.join('')
-        solution = eval(thisJoinedCalculation)
-        runningCalculation = [solution, input]
-        thisCalculation = []
-        thisJoinedCalculation = []
-        displayScreen.textContent = addComma(solution)
-    } else if (input === '/') {
-        console.log(`operator ${input} pressed`)
-        runningCalculation.push(thisJoinedCalculation)
-        thisJoinedCalculation = runningCalculation.join('')
-        solution = eval(thisJoinedCalculation)
-        runningCalculation = [solution, input]
-        thisCalculation = []
-        thisJoinedCalculation = []
-        displayScreen.textContent = addComma(solution)
+    } else if (operators.includes(input)) {
+        // checks if operator was already pressed, if you want to change operator pressing another one will swap them out before continuing the calculation
+        if (runningCalculation.length === 2) {
+            runningCalculation[1] = input
+        } else {
+            console.log(`operator ${input} pressed`)
+            runningCalculation.push(thisJoinedCalculation)
+            thisJoinedCalculation = runningCalculation.join('')
+            solution = eval(thisJoinedCalculation)
+            runningCalculation = [solution, input]
+            thisCalculation = []
+            thisJoinedCalculation = []
+            displayScreen.textContent = addComma(solution)
+        }
+
     } else if (input === "=") {
-        console.log(`operator ${input} pressed`)
-        runningCalculation.push(thisJoinedCalculation)
-        thisJoinedCalculation = runningCalculation.join('')
-        solution = eval(thisJoinedCalculation)
-        runningCalculation = [solution]
-        thisCalculation = []
-        thisJoinedCalculation = []
-        displayScreen.textContent = addComma(solution)
+// shows error message on screen if you attempt to resolve unfinished calculation
+        if (thisJoinedCalculation.length === 0) {
+            displayScreen.textContent = "ERROR"
+        } else if (displayScreen.textContent === "CLEARED") {
+            return;
+        } else {
+            runningCalculation.push(thisJoinedCalculation)
+            thisJoinedCalculation = runningCalculation.join('')
+            solution = eval(thisJoinedCalculation)
+            runningCalculation = [solution]
+            thisCalculation = []
+            thisJoinedCalculation = []
+            displayScreen.textContent = addComma(solution)
+        }
     } else {
         thisCalculation.push(input)
         thisJoinedCalculation = thisCalculation.join('')
         displayScreen.textContent = addComma(thisJoinedCalculation)
     }
+    console.log(thisCalculation, thisJoinedCalculation, runningCalculation)
 }
+
 
 // maps number buttons, assigns event listeners to them
 // runs the appendDigit function, passing in number retrieved from the buttons HTML
